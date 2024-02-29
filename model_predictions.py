893de226f3fa5bc620079f70512b45f356ac9bff
Xuewen Yang <xuewenyang1@gmail.com>
2024-02-29 15:43:16 -0800

    resolved import errors

--- model_predictions.py
+++ model_predictions.py
@@ -6,7 +6,7 @@ from sklearn.feature_selection import SelectFromModel
 from numpy import sort
 from sklearn import metrics
 from sklearn.model_selection import cross_validate
-from importance_explanation import *
+import shap
 
 #helper to avoid using variables that could discriminate against certain groups
 def exclude_columns_with_substrings(df, substrings):
@@ -69,6 +69,27 @@ def evaluate_features(X, y):
     # Print the best threshold, AUC, and accuracy
     print("Best Threshold=%.8f, Best AUC=%.3f, Best Accuracy=%.2f%%" % (best_thresh, best_auc, best_acc*100))
     return best_thresh
+
+def get_top_reasons(shap_values, feature_names, num_reasons=3):
+    top_reasons = []
+    for sv in shap_values:
+        enumerated_list = list(enumerate(sv))
+        sorted_values_indices = sorted(enumerated_list, key=lambda x: x[1], reverse=True)
+        top_num_reasons = sorted_values_indices[:num_reasons]
+        reasons = [(feature_names[idx], value) for idx, value in top_num_reasons]
+        top_reasons.append(reasons)
+    return top_reasons
+    
+def shap_importance(X, selection, selection_model):
+    X_new = exclude_columns_with_substrings(X, ['HEALTHCARE_MEDICAL', 'OTHER_BENEFITS', 'CHILD_DEPENDENTS' ])
+    holdout = selection.transform(X_new)
+    holdout.columns=X_new.columns[selection.get_support()]
+
+    explainer = shap.TreeExplainer(selection_model)
+    shap_values = explainer.shap_values(holdout)
+
+    top_reasons_per_consumer = get_top_reasons(shap_values, holdout.columns)
+    return top_reasons_per_consumer
 
 def run_model(X,y, best_thresh):
 
